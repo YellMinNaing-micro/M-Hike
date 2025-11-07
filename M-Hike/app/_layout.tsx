4c3b60321587211f671e205cad87240039ef6d4a
Yell Min Naing <yellminnaing5@gmail.com>
2025-11-07 11:04:52 +0630

    Update _layout.tsx

--- M-Hike/app/_layout.tsx
+++ M-Hike/app/_layout.tsx
@@ -1,11 +1,14 @@
 import { Stack } from "expo-router";
 import { GluestackUIProvider } from "@gluestack-ui/themed";
 import { config } from "@gluestack-ui/config";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
     return (
-        <GluestackUIProvider config={config}>
-            <Stack screenOptions={{ headerShown: false }} />
-        </GluestackUIProvider>
+        <SafeAreaProvider>
+            <GluestackUIProvider config={config}>
+                <Stack screenOptions={{ headerShown: false }} />
+            </GluestackUIProvider>
+        </SafeAreaProvider>
     );
 }
