9cb2a67a2969803f13981bcc8e08d8726f04972d
Yell Min Naing <yellminnaing5@gmail.com>
2025-11-09 11:26:22 +0630

    Update login.tsx

--- M-Hike/app/login.tsx
+++ M-Hike/app/login.tsx
@@ -14,7 +14,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
 import { Ionicons } from "@expo/vector-icons";
 import { useRouter } from "expo-router";
-import { getUserByCredentials, logAllUsers } from "./database";
+import { getUserByCredentials, logAllUsers } from "../lib/database";
 
 export default function LoginScreen() {
     const router = useRouter();
