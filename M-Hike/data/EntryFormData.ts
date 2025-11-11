87de7cfbc562eedc10983820bd4a4cdbfe88ac24
Yell Min Naing <yellminnaing5@gmail.com>
2025-11-11 16:55:16 +0630

    Create EntryFormData.ts

---
+++ M-Hike/data/EntryFormData.ts
@@ -0,0 +1,13 @@
+export interface EntryFormData {
+    name: string;
+    location: string;
+    length: string;
+    hours: string;
+    minutes: string;
+    hikers: string;
+    animalSightings: string;
+    vegetation: string;
+    weather: string;
+    trail: string;
+    parkingAvailable: boolean;
+}
